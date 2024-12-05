9705df1d08b5516c701bc0f3102805d572b57bcc
Ivan <iievliev.00@gmail.com>
2024-12-05 17:08:18 +0700

    ci.yml

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,7 +6,7 @@ from main import calculate_mortgage
 
 class TestMortgageCalculator(unittest.TestCase):
     def test_calculate_mortgage(self):
-        self.assertAlmostEqual(calculate_mortgage(300000, 5, 30), 1610.46, places=2)
+        self.assertAlmostEqual(calculate_mortgage(30000, 5, 30), 1610.46, places=2)
         self.assertAlmostEqual(calculate_mortgage(100000, 3, 15), 690.58, places=2)
 
     def test_invalid_input(self):
